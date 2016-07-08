87d2e30616a436576272fab1e8cc27177e773424
Shantanu Singh <shsingh@broadinstitute.org>
2016-07-08 15:45:36 -0400

    Update network.tf
    aws_route_table - public

--- network.tf
+++ network.tf
@@ -40,6 +40,36 @@ resource "aws_nat_gateway" "primary" {
   subnet_id = "${aws_subnet.public-us-east-1a.id}"
 }
 
+resource "aws_route_table" "public" {
+    route {
+        cidr_block = "0.0.0.0/16"
+        gateway_id = "${aws_internet_gateway.primary.id}"
+    }
+    tags {
+        Name = "public"
+    }
+    vpc_id = "${aws_vpc.imaging.id}"
+}
+
+resource "aws_route_table_association" "public-us-east-1a" {
+    subnet_id = "${aws_subnet.public-us-east-1a.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public-us-east-1b" {
+    subnet_id = "${aws_subnet.public-us-east-1b.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public-us-east-1d" {
+    subnet_id = "${aws_subnet.public-us-east-1d.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public-us-east-1e" {
+    subnet_id = "${aws_subnet.public-us-east-1e.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
 resource "aws_subnet" "public-us-east-1a" {
   availability_zone = "us-east-1a"
   cidr_block = "10.0.1.0/24"

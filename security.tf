d8c2c228c493b08db2bbfe145e5ee062f5fce9cd
Allen Goodman <allen.goodman@icloud.com>
2016-07-08 12:44:29 -0400

    MySQL security policy

---
+++ security.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "MySQL" {
+  description = "MySQL"
+  egress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+  }
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 0
+    protocol = "TCP"
+    to_port = 3306
+  }
+  name = "MySQL"
+  vpc_id = "${aws_vpc.imaging.id}"
+}
